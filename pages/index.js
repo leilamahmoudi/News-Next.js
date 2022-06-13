5d3b473b609bc8779d769442da9a0193857b45c0
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2022-06-13 11:29:36 +0200

    added toolbar

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,12 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div className="page-container">
+      <div className={styles.main}>
+        <h1>Next.js News app</h1>
+        <h3>Latest news articles</h3>
+      </div>
+    </div>
+  );
 }
