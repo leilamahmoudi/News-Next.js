fd485f8d19e53372d4f9941d817ca4d2782bc057
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2022-06-14 16:26:57 +0200

    added some styles

--- components/toolbar.js
+++ components/toolbar.js
@@ -7,7 +7,7 @@ export const Toolbar = () => {
     <div className={styles.main}>
       <div onClick={() => router.push("/")}>Home</div>
       <div onClick={() => router.push("/feed/1")}>Feed</div>
-      <div onClick={() => router.push("/eom")}>EOM</div>
+      <div onClick={() => router.push("/nextjs")}>NEXT.JS</div>
       <div
         onClick={() => (window.location.href = "https://twitter.com/leillonn")}
       >
