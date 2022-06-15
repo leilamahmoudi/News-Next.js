b415efa28d374df7a0a5dcc50ec7bf7f50750eb0
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2022-06-15 16:49:31 +0200

    added background to landing page and styles

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,14 @@ export default function Home() {
     <div className="page-container">
       <Toolbar />
       <div className={styles.main}>
-        <h1>Next.js News app</h1>
-        <h3>Latest news articles1</h3>
         <video autoPlay loop muted className={styles.video}>
-          <source src="/img/production.mp4" type="video/mp4" />
+          <source src="/img/test.mp4" type="video/mp4" />
         </video>
+        <div className={styles.overlay}></div>
+        <div className={styles.text}>
+          <h1>Next.js News app</h1>
+          <h3>Latest news articles</h3>
+        </div>
       </div>
     </div>
   );
