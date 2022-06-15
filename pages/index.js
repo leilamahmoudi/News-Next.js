0e715cbaf4d7ee905bcefb0a664c446bffb85352
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2022-06-15 15:34:34 +0200

    added background to landing page

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,10 @@ export default function Home() {
       <Toolbar />
       <div className={styles.main}>
         <h1>Next.js News app</h1>
-        <h3>Latest news articles</h3>
-        <img src="https://media4.s-nbcnews.com/i/newscms/2019_01/2705191/nbc-social-default_b6fa4fef0d31ca7e8bc7ff6d117ca9f4.png" />
+        <h3>Latest news articles1</h3>
+        <video autoPlay loop muted className={styles.video}>
+          <source src="/img/production.mp4" type="video/mp4" />
+        </video>
       </div>
     </div>
   );
