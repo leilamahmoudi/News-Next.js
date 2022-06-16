29d8c42fc3f60a132fb91f4a0d64dcccdf420225
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2022-06-16 10:48:56 +0200

    edited toolbar

--- components/toolbar.js
+++ components/toolbar.js
@@ -7,11 +7,18 @@ export const Toolbar = () => {
     <div className={styles.main}>
       <div onClick={() => router.push("/")}>Home</div>
       <div onClick={() => router.push("/feed/1")}>Feed</div>
-      <div onClick={() => router.push("/nextjs")}>NEXT.JS</div>
       <div
         onClick={() => (window.location.href = "https://twitter.com/leillonn")}
       >
         Twitter
+      </div>
+      <div
+        onClick={() =>
+          (window.location.href =
+            "https://www.linkedin.com/in/leila-mahmoudi-96a417202/")
+        }
+      >
+        linkedin
       </div>
     </div>
   );
